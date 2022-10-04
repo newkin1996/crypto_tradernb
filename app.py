dd99f714f3f3c219df13f58ecfb960bf19936c24
dinithidulmini <dinithi.cima@gmail.com>
2022-10-04 17:24:01 +0400

    10-02

--- app.py
+++ app.py
@@ -537,14 +537,6 @@ def get_futures():
         except:
             return render_template('invalid_key.html')
 
-        open_orders = client.futures_get_open_orders()
-        print(f"oo = {open_orders}")
-        for oo in open_orders:
-            if oo["symbol"] == "DOTUSDT":
-                oo_order_id = oo["orderId"]
-                cancel_futures_oo = client.futures_cancel_order(symbol="DOTUSDT", orderId=oo_order_id)
-                print(f"oo order cancelled = {cancel_futures_oo}")
-
         try:
 
             open_orders = client.futures_get_open_orders()
